7befc6fcfd3fc48c8b54693032679720d25ec1a1
Kevin Kirkpatrick <your.email@example.com>
2025-03-10 14:06:02 -0700

    Update CORS and build configuration for embedding

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,27 @@
 import { defineConfig } from 'vite'
 import path from 'path'
+import react from "@vitejs/plugin-react-swc"
 
 export default defineConfig({
   server: {
     cors: {
-      origin: "https://www.sunsama.com",
+      origin: "*",
       credentials: true
     }
+  },
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/embed.tsx'),
+      name: 'BurnoutCalculator',
+      fileName: (format) => `burnout-calculator.${format}.js`,
+      formats: ['umd', 'es'],
+    },
+    rollupOptions: {
+      output: {
+        inlineDynamicImports: true,
+      },
+    },
+    cssCodeSplit: false,
   },
   resolve: {
     alias: {
